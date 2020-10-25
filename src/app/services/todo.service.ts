9363ae1b6cb765ea456e812afaa47d0a981c70c7
Senott <senott@yahoo.com>
2020-10-25 10:45:42 -0300

    Getting todo items from 'https://jsonplaceholder.typicode.com/todos'.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Todo } from '../models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=10'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'First Todo',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Second Todo',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Third Todo',
-        completed: false,
-      }
-    ];
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
