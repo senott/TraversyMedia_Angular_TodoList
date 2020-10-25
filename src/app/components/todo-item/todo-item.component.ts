9628ca430716b7a5aa9dc94a80cfdaebcba9b158
Senott <senott@yahoo.com>
2020-10-25 10:16:35 -0300

    project configuration, Todos and Todo Item components.

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Set dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(Todo) {
+    this.todo.completed = !this.todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
+
+}
